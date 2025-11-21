ae68855e468d237fdb6d44be2a2b30b2595bc53b
Raffles Zhu <47984565+zhux002@users.noreply.github.com>
2025-11-21 16:30:20 -0500

    update

---
+++ pikaraoke/dichoptic-karaoke-demo-main/lyricDict.py
@@ -0,0 +1,4 @@
+SongNameToLyicsFilePath = {
+    "Backstreet Boys - I Want It That Way (Karaoke Version)---NxilU56kPu0.mp4": "IReadItThatWay_lyrics.csv",
+    "Neil Diamond  - Sweet Caroline (Karaoke Version)---srLoAl1mhFw.mp4": "sweetparadigm_lyrics.csv",
+}
